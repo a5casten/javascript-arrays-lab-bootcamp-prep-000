3828b4e19e89d1c796c4149c35c26e4f2714aad4
Amanda Casten <ascasten@gmail.com>
2020-06-29 23:56:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var kittens = ["Milo","Otis","Garfield"]; //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name){
-  
+  kittens.push(name);
+  return kittens;
 }
 
 function destructivelyPrependKitten(name){
-  return (name, ...kittens)
+  kittens.unshift(name);
+  return kittens;
 }
 
 function destructivelyRemoveLastKitten() {
